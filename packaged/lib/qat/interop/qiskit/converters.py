f7a96c72efa14cd34e2d08b1b7d8830139b2784b
Reda DRISSI <mohamed-reda.drissi@atos.net>
2019-05-10 11:17:40 +0200

    [[FEATURE:ADDED]]moved qiskit converters to a folder and added qiskit qpu, and qlm backend for qiskit

---
+++ packaged/lib/qat/interop/qiskit/converters.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+"""
+@brief
+
+@namespace ...
+@authors Reda Drissi <mohamed-reda.drissi@atos.net>
+@copyright 2019  Bull S.A.S.  -  All rights reserved.
+           This is not Free or Open Source software.
+           Please contact Bull SAS for details about its license.
+           Bull - Rue Jean Jaurès - B.P. 68 - 78340 Les Clayes-sous-Bois
+
+
+Description Converts qiskit circuit into a qlm circuit object,
+            you can use : qlm_circuit = to_qlm_circ(your_qiskit_circuit)
+            This is a placeholder, names and packaging might change to
+            keep consistency
+
+Overview
+=========
+
+
+"""
+
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer
+from qat.lang.AQASM import *
+from qat.lang.AQASM.gates import *
+from qat.comm.shared.ttypes import Job, Batch, Result
+from qat.comm.datamodel.ttypes import Circuit
+import numpy as np
+
+
+def get_qindex(circ, name, index):
+    """ Find the qubit index"""
+    ret = 0
+    for reg in circ.qregs:
+        if name != reg.name:
+            ret += reg.size
+        else:
+            return ret + index
+
+
+def get_cindex(circ, name, index):
+    """ Find the classical bit index"""
+    ret = 0
+    for reg in circ.cregs:
+        if name != reg.name:
+            ret += reg.size
+        else:
+            return ret + index
+# Let's add the U gate, u1/2/3 would be dealt with through setting
+# the appropriate params to 0
+def gen_U(theta, phi, lamda):
+    """ generate the U gate matrix """
+    m11 = (np.e ** (1j*(phi + lamda)/2)) * np.cos(theta/2)
+    m12 = (-1) * (np.e ** (1j*(phi - lamda)/2)) * np.sin(theta/2)
+    m21 = (np.e ** (1j*(phi - lamda)/2)) * np.sin(theta/2)
+    m22 = (np.e ** (1j*(phi + lamda)/2)) * np.cos(theta/2)
+    return np.array([[m11, m12],[m21, m22]], dtype = np.complex128)
+
+def gen_RZZ(theta):
+    """ generates the RZZ gate matrix """
+    return np.diag([1, np.exp(1j*theta), np.exp(1j*theta), 1])
+
+U = AbstractGate("U", [float]*3, arity=1,
+                 matrix_generator=gen_U)
+RZZ = AbstractGate("RZZ", [float], arity=2,
+                   matrix_generator=gen_RZZ)
+# get qbits
+
+def process_U(params):
+    """ return corresponding U matrix"""
+    return U(params[0], params[1], params[2])
+
+def process_U2(params):
+    """ Returns the corresponding u2 matrix """
+    return U(0, params[0], params[1])
+
+gate_dic={'h': H, 'x': X, 'y': Y, 'z': Z, 'xbase': X, 'swap': SWAP,
+          'id': I, 's': S, 'sdg': S.dag(), 't': T, 'tdg': T.dag(),
+          'rx': RX, 'ry': RY, 'rz': RZ, 'rzz': RZZ, 'u0': I, 'u1': RY,
+          'u2': process_U2, 'u3': process_U, 'U': process_U}
+
+def get_gate(gate, params):
+    """ generates pyAQASM corresponding gate """
+    if gate =="u0":
+        return I
+    elif gate[0] == 'c':
+        return get_gate(gate[1:], params).ctrl()
+    elif len(params)==0:
+        return gate_dic[gate]
+    elif len(params)==1:
+        return gate_dic[gate](params[0])
+    else:
+        return gate_dic[gate](params)
+
+def to_qlm_circ(qiskit_circuit, sep_measure=False):
+    """ translates a qiskit circuit into a qlm circuit"""
+    prog = Program()
+    qbits_num = 0
+    to_measure = []
+    for reg in qiskit_circuit.qregs:
+        qbits_num += qbits_num + reg.size
+    qbits = prog.qalloc(qbits_num)
+
+    cbits_num = 0
+    for reg in qiskit_circuit.cregs:
+        cbits_num += cbits_num + reg.size
+    cbits = prog.calloc(cbits_num)
+    for op in qiskit_circuit.data:
+        qb = [] # qbits arguments
+        cb = [] # cbits arguments
+        prms = [] # gate parameters
+        # Get qbit arguments
+        for reg in op.qargs:
+            qb.append(qbits[get_qindex(qiskit_circuit, reg[0].name, reg[1])])
+
+        # Get cbit arguments
+        for reg in op.cargs:
+            cb.append(qbits[get_cindex(qiskit_circuit, reg[0].name, reg[1])])
+
+        # Get parameters
+        for p in op.param:
+            prms.append(float(p))
+        # Apply measure #
+        if op.name == 'measure':
+            if sep_measure:
+                to_measure.append(qb)
+            else:
+                prog.measure(qb, cb)
+        else:
+            # Apply gates #
+            prog.apply(get_gate(op.name, prms), qb)
+    if sep_measure:
+        return prog.to_circ(), to_measure
+    else:
+        return prog.to_circ()
+def qlm_circ_sep_meas(qiskit_circuit):
+    return to_qlm_circ(qiskit_circuit, True)
+
+def gen_qiskit_gateset(qc):
+    return {H: qc.h, X: qc.x, Y: qc.y, Z: qc.z, SWAP: qc.swap,
+            I: qc.id, S: qc.s, S.dag(): qc.sdg, T: qc.t,
+            T.dag(): qc.tdg, RX: qc.rx, RY: qc.ry, RZ: qc.rz}
+
+def to_qiskit_circuit(qlm_circuit):
+    qreg = QuantumRegister(qlm_circuit.nbqbits)
+    creg = ClassicalRegister(qlm_circuit.nbcbits)
+    qc = QuantumCircuit(qreg, creg)
+    dic = gen_qiskit_gateset(qc)
+    for op in qlm_circuit.ops:
+        if op.type == 0:
+            try:
+                dic[op.gate](op.gate.parameters + [qreg[i.index] for i in op.qbits])
+            except KeyError:
+                raise ValueError("Gate {} not supported by qiskit API".format(op.gate.name))
+        elif op.type == 1:
+            for index in range(len(op.qbits)):
+                qc.measure(op.qbits[index], op.cbits[index])
+    return qc
+
+def job_to_qiskit_circuit(qlm_job):
+    return to_qiskit_circuit(qlm_job.circuit)
---
+++ packaged/lib/qat/interop/qiskit/providers.py
@@ -0,0 +1,160 @@
+from qiskit.providers import BaseBackend, BaseJob
+from qiskit.providers.models.backendconfiguration import BackendConfiguration, GateConfig
+from qiskit.result import Result
+from qiskit.result.models import ExperimentResult, ExperimentResultData
+from qiskit.converters import qobj_to_circuits
+from qiskit.validation.base import Obj
+from qiskit import execute
+# QLM imports
+from .converters import qlm_circ_sep_meas
+from .converters import job_to_qiskit_circuit
+from qat.comm.shared.ttypes import Job, Batch
+from qat.core.qpu.qpu import QPUHandler
+from collections import Counter
+
+def to_string(state, nbqbits):
+    st = bin(state)[2:]
+    st = "0" * (nbqbits - len(st)) + st
+    return st
+
+def generate_experiment_result(qlm_result, nbqbits, head):
+    """
+    Generates an experiment result.
+    Returns a qiskit.ExperimentResult structure.
+    """
+    counts = dict()
+    samples = [hex(s.state) for s in qlm_result.raw_data]
+    counts = dict(Counter(samples))
+    data = ExperimentResultData.from_dict({'counts':counts})
+    return ExperimentResult(shots=len(qlm_result.raw_data),
+                            success=True,
+                            data=data,
+                            header=Obj.from_dict(head))
+
+
+def generate_result(backend_name, backend_version, qobj_id, job_id, success, qlm_results, n_list, headers):
+    """
+    Tranform a QLM result into a qiskit result structure.
+    Returns a qiskit.Result structure.
+    """
+    return Result(backend_name=backend_name,
+                  backend_version=backend_version,
+                  qobj_id=qobj_id,
+                  job_id=job_id,
+                  success=success,
+                  results=[generate_experiment_result(result, n, head)\
+                          for result, n, head in zip(qlm_results, n_list, headers)])
+
+
+class QLMJob(BaseJob):
+    """
+    QLM Job.
+    QLM Job implement the required BaseJob interface of Qiskit with a small twist:
+    everything is computed synchronously (meaning that the job is stored at submit and
+    computed at result).
+    """
+
+    def add_results(self, qlm_result, qobj_id, n_list, headers):
+        self._results = generate_result(self._backend._configuration.backend_name,
+                                        self._backend._configuration.backend_version,
+                                        qobj_id,
+                                        self._job_id,
+                                        True,
+                                        qlm_result,
+                                        n_list, headers)
+    def status(self):
+        pass
+
+    def cancel(self):
+        pass
+
+    def submit(self):
+        pass
+
+    def result(self):
+        return self._results
+
+## TODO :  can be improved by publishing any pyAQASM abstract gate via its circuit implementation.
+## For now lets stick with the QASM usual gate set
+
+_QLM_GATE_NAMES = [
+        "id", "u0", "u1", "u2", "u3", "x", "y", "z", "h", "s",
+        "sdg", "t", "tdg", "rx", "ry", "rz", "cx", "cy", "cz",
+        "ch", "crz", "cu1", "cu3", "swap", "ccx", "cswap"]
+
+_QLM_GATES = [GateConfig(name="FOO", parameters=[], qasm_def="BAR")]
+
+_QLM_PARAMS = {
+        "backend_name":"QLMConnector", # Name of the back end
+        "backend_version":"0.0.1", # Version of the back end
+        "n_qubits":100, # Nb qbits
+        "basis_gates":_QLM_GATE_NAMES, # We accept all the gates of Qiskit (and more, but hey..)
+        "gates":_QLM_GATES, # They don't even use it for their simulators, so...
+        "local":True, # Its a local backend
+        "simulator":True, # Its a simulator. Is it though?
+        "conditional":True, # We support conditionals
+        "open_pulse":False, # We do not support open Pulse
+        "memory":False, # We do not support Memory (wth?)
+        "max_shots":4096} # Max shots is 4096 (required :/)
+
+class NoQpuAttached(Exception):
+    pass
+
+_QLM_BACKEND = BackendConfiguration(**_QLM_PARAMS)
+class QLMBackend(BaseBackend):
+    """
+    Basic connector implementing a Qiskit Backend, plugable on a QLM QPUHandler.
+    """
+    def __init__(self, configuration, provider=None):
+        super(QLMBackend, self).__init__(configuration, provider)
+        self.id_counter = 0
+        self._qpu = None
+
+    def set_qpu(self, qpu):
+        self._qpu = qpu
+
+    def run(self, qobj):
+        if self._qpu is None:
+            raise NoQpuAttached("No qpu attached to the QLM connector.")
+        headers = [exp.header.as_dict() for exp in qobj.experiments]
+        circuits = qobj_to_circuits(qobj)
+        nbshots = qobj.config.shots
+        qlm_task = Batch(jobs=[])
+        n_list = []
+        for circuit in circuits:
+            qlm_circuit, to_measure = qlm_circ_sep_meas(circuit)
+            job = qlm_circuit.to_job()
+            job.nbshots = nbshots
+            job.qubits = [q for q,c in to_measure]
+            n_list.append(len(to_measure))
+            qlm_task.jobs.append(job)
+
+        results = self._qpu.submit(qlm_task)
+        #Creating a job that will contain the results
+        job = QLMJob(self, str(self.id_counter))
+        self.id_counter += 1
+        job.add_results(results, qobj.qobj_id, n_list, headers)
+        return job
+
+
+class QiskitQPU(QPUHandler):
+
+    def __init__(self, backend=None, plugins=None):
+        super(QPUHandler, self).__init__(plugins)
+        self.backend = backend
+
+    def set_backend(self, backend):
+        self.backend = backend
+
+    def submit_job(self, qlm_job):
+        qiskit_circuit = job_to_qiskit_circuit(qlm_job)
+        return execute(qiskit_circuit, self).result()
+
+
+class QLMConnector:
+    def __or__(self, qpu):
+        backend = QLMBackend(_QLM_BACKEND)
+        backend.set_qpu(qpu)
+        return backend
+
+QLMConnector = QLMConnector()
